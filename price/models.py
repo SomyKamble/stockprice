1d872cd4b77d4093f11b9012139cae32b69dadac
somykamble <somykamble1001@gmail.com>
2020-03-19 13:54:05 +0530

    changes in the requirement.txt

--- price/models.py
+++ price/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Product(models.Model):
+    tweets_uname=models.CharField(max_length=500)
+
+    tweet_info=models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.tweet_info
+
+
